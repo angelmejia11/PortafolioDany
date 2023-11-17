ff961caa38ae4b63f4e837ed445f72f91091a766
Angelmejia011 <amejia.dacompsc@gmail.com>
2023-11-16 23:20:02 -0600

    Responsivo

---
+++ javaScript.js
@@ -0,0 +1,9 @@
+//Funcion mostrar el menú resposive
+function responsiveMenu(){
+    var x= document.getElementById("nav");
+    if(x.className===""){
+       x.className = "responsive"; 
+    }else{
+        x.className = "";
+    }
+}
