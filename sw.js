50f0ba515bf55ef499350c3440208f0768e05ad7
Angelmejia011 <amejia.dacompsc@gmail.com>
2023-11-30 16:55:26 -0600

    PWA segundo intento

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ const archivosCache=[
 ]
 
 
-self.addEventListener('install', e => {
+/*self.addEventListener('install', e => {
     console.log("Service worker se instalo correctamente", e);
     e.waitUntil(
         caches.open(nombreCache).then((cache)=>{
@@ -34,4 +34,51 @@ self.addEventListener('install', e => {
             return respuestaCache 
         })
     )
- });
+ });*/
+
+ self.addEventListener('install', e => {
+    console.log("El SW se instaló", e);
+    e.waitUntil(
+        caches.open(nombreCache)
+        .then((cache) => {
+            console.log();
+            return cache.addAll(archivosCache);
+        })
+        .catch(error => {
+            console.error('Error durante la instalación del caché:', error);
+        })
+    );
+});
+
+self.addEventListener('fetch', e => {
+    console.log("Fetch...", e);
+    e.respondWith(
+        caches.match(e.request)
+        .then(respuestaCache => {
+            return respuestaCache || fetch(e.request)
+                .then(respuestaRed => {
+                    return caches.open(nombreCache)
+                        .then(cache => {
+                            cache.put(e.request, respuestaRed.clone());
+                            return respuestaRed;
+                        });
+                })
+                .catch(error => {
+                    console.error('Error al intentar recuperar el recurso:', error);
+                });
+        })
+    );
+});
+
+self.addEventListener('activate', e => {
+    console.log("El SW está activo", e);
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.map(key => {
+                if (key !== nombreCache) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
